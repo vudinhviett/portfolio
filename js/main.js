74ea784cd3185f687201fc458c32ea3d6c89f063
Vu Viet <vudinhviett@gmail.com>
2021-10-31 23:25:52 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
 const menu = document.querySelector(".navbar-icon");
 const list = document.querySelector(".navbar");
 const navItem = document.querySelectorAll(".navbar-item");
-
 menu.addEventListener("click", function () {
   list.classList.add("active");
-
   if (menu.classList.contains("active")) {
     menu.classList.add("fa-bars");
     menu.classList.remove("fa-times-circle");
@@ -16,7 +14,6 @@ menu.addEventListener("click", function () {
     menu.classList.add("fa-times-circle");
   }
 });
-
 for (item of navItem) {
   item.addEventListener("click", function () {
     menu.classList.add("fa-bars");
@@ -25,12 +22,10 @@ for (item of navItem) {
     list.classList.remove("active");
   });
 }
-
 //Scroll
 var sections = document.querySelectorAll("section");
 onscroll = function () {
   var top = document.documentElement.scrollTop;
-
   sections.forEach((section) => {
     var height = section.offsetHeight;
     var offset = section.offsetTop - 100;
@@ -46,3 +41,27 @@ function activeItem(id) {
   document.querySelector(`a[href='#${id}']`).classList.add("active-color");
 }
 
+function animateNumber(finalNumber, duration = 5000, startNumber = 0, callback) {
+  let currentNumber = startNumber
+  const interval = window.setInterval(updateNumber, 17)
+  function updateNumber() {
+    if (currentNumber >= finalNumber) {
+      clearInterval(interval)
+    } else {
+      let inc = Math.ceil(finalNumber / (duration / 17))
+      if (currentNumber + inc > finalNumber) {
+        currentNumber = finalNumber
+        clearInterval(interval)
+      } else {
+        currentNumber += inc
+      }
+      callback(currentNumber)
+    }
+  }
+}
+document.addEventListener('DOMContentLoaded', function () {
+  animateNumber(19666, 3000, 0, function (number) {
+    const formattedNumber = number.toLocaleString()
+    document.getElementById('transaction-count').innerText = formattedNumber
+  })
+})
