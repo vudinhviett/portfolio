f1fa3d614499c5899c1f09a9d71163a491379bac
Vu Viet <vudinhviett@gmail.com>
2021-12-18 15:02:20 +0700

    update

--- js/main.js
+++ js/main.js
@@ -60,7 +60,7 @@ function animateNumber(finalNumber, duration = 5000, startNumber = 0, callback) 
     }
 }
 document.addEventListener('DOMContentLoaded', function () {
-    animateNumber(21500, 1500, 0, function (number) {
+    animateNumber(24000, 1500, 0, function (number) {
         const formattedNumber = number.toLocaleString()
         document.getElementById('transaction-count').innerText = formattedNumber
     })
