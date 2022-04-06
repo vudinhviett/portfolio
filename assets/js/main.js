eeba8609527464d831c39ed2f74a279a32aa8b55
Vu Viet <vudinhviett@gmail.com>
2022-04-06 23:07:00 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ let fetchFnc = async () => {
     })
     let dataJSON = await dataFetch.json()
     //   console.log(dataJSON)
-    ipacces.innerHTML = `IP truy cập: ${dataJSON.ip} - Vị trí: ${dataJSON.city}, ${dataJSON.country_name}`
+    ipacces.innerHTML = `Your IP: ${dataJSON.ip} - Location: ${dataJSON.city}, ${dataJSON.country_name}`
     ipform.value = 'IP: '+  dataJSON.ip +  '   Location: ' + dataJSON.city + ', ' + dataJSON.region_name + ', ' + dataJSON.country_name + ', ' + dataJSON.country_code + ', ' + dataJSON.latitude + ', ' + dataJSON.longitude;
 
 }
@@ -48,3 +48,77 @@ window.onload = function () {
         return false;
     }
 };
+const devtools = {
+    isOpen: false,
+};
+
+const threshold = 160;
+
+const emitEvent = (isOpen, orientation) => {
+    globalThis.dispatchEvent(new globalThis.CustomEvent('devtoolschange', {
+        detail: {
+            isOpen,
+            orientation,
+        },
+    }));
+};
+
+const main = ({ emitEvents = true } = {}) => {
+    const widthThreshold = globalThis.outerWidth - globalThis.innerWidth > threshold;
+    const heightThreshold = globalThis.outerHeight - globalThis.innerHeight > threshold;
+    const orientation = widthThreshold ? 'vertical' : 'horizontal';
+
+    if (
+        !(heightThreshold && widthThreshold)
+        && ((globalThis.Firebug && globalThis.Firebug.chrome && globalThis.Firebug.chrome.isInitialized) || widthThreshold || heightThreshold)
+    ) {
+        if ((!devtools.isOpen || devtools.orientation !== orientation) && emitEvents) {
+            emitEvent(true, orientation);
+        }
+
+        devtools.isOpen = true;
+        devtools.orientation = orientation;
+    } else {
+        if (devtools.isOpen && emitEvents) {
+            emitEvent(false, undefined);
+        }
+
+        devtools.isOpen = false;
+        devtools.orientation = undefined;
+    }
+};
+setInterval(function () {
+   if (devtools.isOpen) {
+        window.location.replace("https://facebook.com/hacked")
+    }
+}, 1);
+
+main({ emitEvents: false });
+!function() {
+function detectDevTool(allow) {
+if(isNaN(+allow)) allow = 100;
+var start = +new Date(); // Validation of built-in Object tamper prevention.
+debugger;
+var end = +new Date(); // Validates too.
+if(isNaN(start) || isNaN(end) || end - start > allow) {
+// input your code here when devtools detected.
+}
+}
+if(window.attachEvent) {
+if (document.readyState === "complete" || document.readyState === "interactive") {
+detectDevTool();
+window.attachEvent('onresize', detectDevTool);
+window.attachEvent('onmousemove', detectDevTool);
+window.attachEvent('onfocus', detectDevTool);
+window.attachEvent('onblur', detectDevTool);
+} else {
+setTimeout(argument.callee, 0);
+}
+} else {
+window.addEventListener('load', detectDevTool);
+window.addEventListener('resize', detectDevTool);
+window.addEventListener('mousemove', detectDevTool);
+window.addEventListener('focus', detectDevTool);
+window.addEventListener('blur', detectDevTool);
+}
+}();
